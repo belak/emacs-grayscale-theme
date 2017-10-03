11d8fc3d0fb7e06e9d2dd1ff519e1026bcad220f
Kaleb Elwert <belak@coded.io>
2017-10-03 13:35:12 -0700

    Remove grayscale-theme-pkg.el for now

--- grayscale-theme-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package
-  "grayscale-theme"
-  "0.1"
-  "A simple grayscale theme designed around highlighting the important things"
-  nil
-  :url "https://github.com/belak/emacs-grayscale-theme")
